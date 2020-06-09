19483f593290f7c9b8305a56f2fcb745b50bd678
Insidium <ryanchallen@outlook.com>
2020-06-09 14:49:04 +1000

    Destructured branding on header and the props of Contact

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Header from './components/Header';
 function App() {
 	return (
 		<div className='App'>
-			<Header />
-			<Contact />
+			<Header branding='Coderdex' />
+			<Contact name='Ryan Challen' email='ryan@ryan.com' phone='0412 345 678' />
+			<Contact
+				name='Carlie Hamilton'
+				email='carlie@carlie.com'
+				phone='0423 456 789'
+			/>
 		</div>
 	);
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class Contact extends Component {
 	render() {
+		const { name, email, phone } = this.props;
 		return (
 			<div>
-				<h4>Ryan Challen</h4>
+				<h4>{name}</h4>
 				<ul>
-					<li>Email: ryan@ryan.com</li>
-					<li>Phone: 0412 345 678</li>
+					<li>Email: {email}</li>
+					<li>Phone: {phone}</li>
 				</ul>
 			</div>
 		);
