04772f3716d83bf2ea50fa4f27bbccf5a04dc02f
Insidium <ryanchallen@outlook.com>
2020-06-09 14:37:16 +1000

    Added first functional component, a Header, to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>The App Component</h1>
+			<Header />
 			<Contact />
 		</div>
 	);
