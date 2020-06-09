abd38571a915daaf7e0c8870b31e8a0816f59df4
Insidium <ryanchallen@outlook.com>
2020-06-09 15:56:57 +1000

    Installed bootstrap for styling components, created navbar with Home item to start with, added hard coded contacts to cards, wrapped body content in container

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 function App() {
 	return (
 		<div className='App'>
 			<Header branding='Coderdex' />
-			<Contact name='Ryan Challen' email='ryan@ryan.com' phone='0412 345 678' />
-			<Contact
-				name='Carlie Hamilton'
-				email='carlie@carlie.com'
-				phone='0423 456 789'
-			/>
+			<div className='container'>
+				<Contact
+					name='Ryan Challen'
+					email='ryan@ryan.com'
+					phone='0412 345 678'
+				/>
+				<Contact
+					name='Carlie Hamilton'
+					email='carlie@carlie.com'
+					phone='0423 456 789'
+				/>
+			</div>
 		</div>
 	);
 }
