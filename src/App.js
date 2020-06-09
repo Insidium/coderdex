e9f927cf18cd41b9a5a1316d6cbf1a4873a85c3a
Insidium <ryanchallen@outlook.com>
2020-06-09 14:32:50 +1000

    Created Contact component and imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Contact from './components/Contact';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>The App Component</h1>
+			<Contact />
 		</div>
 	);
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<h4>Ryan Challen</h4>
+				<ul>
+					<li>Email: ryan@ryan.com</li>
+					<li>Phone: 0412 345 678</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Contact;
