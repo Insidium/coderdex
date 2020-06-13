8c28187d2d718e5da34f04ebd0b6323b35aa564d
Insidium <ryanchallen@outlook.com>
2020-06-13 13:22:52 +1000

    Added onChange function to form inputs to allow for values to register, added onSubmit function to set states to values inputted

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,13 +6,21 @@ class AddContact extends Component {
 		email: '',
 		phone: ''
 	};
+
+	onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		console.log(this.state);
+	};
+
 	render() {
 		const { name, email, phone } = this.state;
 		return (
 			<div className='card mb3'>
 				<div className='card-header'>Add Contact</div>
 				<div className='card-body'>
-					<form>
+					<form onSubmit={this.onSubmit}>
 						<div className='form-group'>
 							<label htmlFor='name'>Name</label>
 							<input
@@ -21,6 +29,7 @@ class AddContact extends Component {
 								className='form-control form-control-lg'
 								placeholder='Enter name...'
 								value={name}
+								onChange={this.onChange}
 							/>
 						</div>
 						<div className='form-group'>
@@ -31,6 +40,7 @@ class AddContact extends Component {
 								className='form-control form-control-lg'
 								placeholder='Enter email...'
 								value={email}
+								onChange={this.onChange}
 							/>
 						</div>
 						<div className='form-group'>
@@ -41,6 +51,7 @@ class AddContact extends Component {
 								className='form-control form-control-lg'
 								placeholder='Enter phone number...'
 								value={phone}
+								onChange={this.onChange}
 							/>
 						</div>
 						<input
