136c97c097da6978ef3e1ebfced41f3ef8573d43
Insidium <ryanchallen@outlook.com>
2020-06-13 10:01:32 +1000

    Created AddContact component with form for contact details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +13,7 @@ function App() {
 			<div className='App'>
 				<Header branding='Coderdex' />
 				<div className='container'>
+					<AddContact />
 					<Contacts />
 				</div>
 			</div>
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+	state = {
+		name: '',
+		email: '',
+		phone: ''
+	};
+	render() {
+		return (
+			<div className='card mb3'>
+				<div className='card-header'>Add Contact</div>
+				<div className='card-body'>
+					<form>
+						<div className='form-group'>
+							<label htmlFor='name'>Name</label>
+							<input
+								type='text'
+								name='name'
+								className='form-control form-control-lg'
+								placeholder='Enter name...'
+							/>
+						</div>
+						<div className='form-group'>
+							<label htmlFor='email'>Email</label>
+							<input
+								type='email'
+								name='email'
+								className='form-control form-control-lg'
+								placeholder='Enter email...'
+							/>
+						</div>
+						<div className='form-group'>
+							<label htmlFor='phone'>Phone</label>
+							<input
+								type='text'
+								name='phone'
+								className='form-control form-control-lg'
+								placeholder='Enter phone number...'
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default AddContact;
