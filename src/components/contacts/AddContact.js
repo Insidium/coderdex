3090046b49ad8466a170860f495ea61ca1067d44
Insidium <ryanchallen@outlook.com>
2020-06-14 14:08:29 +1000

    Added Add Contact to context reducer, added dispatch to AddContact and installed uuid

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
-import uuid from 'uuid';
+import { v1 as uuid } from 'uuid';
 
 class AddContact extends Component {
 	state = {
@@ -13,12 +13,17 @@ class AddContact extends Component {
 
 	onSubmit = (dispatch, e) => {
 		e.preventDefault();
+
 		const { name, email, phone } = this.state;
+
 		const newContact = {
+			id: uuid(),
 			name,
 			email,
 			phone
 		};
+
+		dispatch({ type: 'ADD_CONTACT', payload: newContact });
 	};
 
 	render() {
@@ -27,52 +32,54 @@ class AddContact extends Component {
 		return (
 			<Consumer>
 				{(value) => {
-					const dispatch = value;
-					<div className='card mb3'>
-						<div className='card-header'>Add Contact</div>
-						<div className='card-body'>
-							<form onSubmit={this.onSubmit.bind(this, dispatch)}>
-								<div className='form-group'>
-									<label htmlFor='name'>Name</label>
+					const { dispatch } = value;
+					return (
+						<div className='card mb3'>
+							<div className='card-header'>Add Contact</div>
+							<div className='card-body'>
+								<form onSubmit={this.onSubmit.bind(this, dispatch)}>
+									<div className='form-group'>
+										<label htmlFor='name'>Name</label>
+										<input
+											type='text'
+											name='name'
+											className='form-control form-control-lg'
+											placeholder='Enter name...'
+											value={name}
+											onChange={this.onChange}
+										/>
+									</div>
+									<div className='form-group'>
+										<label htmlFor='email'>Email</label>
+										<input
+											type='email'
+											name='email'
+											className='form-control form-control-lg'
+											placeholder='Enter email...'
+											value={email}
+											onChange={this.onChange}
+										/>
+									</div>
+									<div className='form-group'>
+										<label htmlFor='phone'>Phone</label>
+										<input
+											type='text'
+											name='phone'
+											className='form-control form-control-lg'
+											placeholder='Enter phone number...'
+											value={phone}
+											onChange={this.onChange}
+										/>
+									</div>
 									<input
-										type='text'
-										name='name'
-										className='form-control form-control-lg'
-										placeholder='Enter name...'
-										value={name}
-										onChange={this.onChange}
+										type='submit'
+										value='Add Contact'
+										className='btn btn-light btn-block'
 									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='email'>Email</label>
-									<input
-										type='email'
-										name='email'
-										className='form-control form-control-lg'
-										placeholder='Enter email...'
-										value={email}
-										onChange={this.onChange}
-									/>
-								</div>
-								<div className='form-group'>
-									<label htmlFor='phone'>Phone</label>
-									<input
-										type='text'
-										name='phone'
-										className='form-control form-control-lg'
-										placeholder='Enter phone number...'
-										value={phone}
-										onChange={this.onChange}
-									/>
-								</div>
-								<input
-									type='submit'
-									value='Add Contact'
-									className='btn btn-light btn-block'
-								/>
-							</form>
+								</form>
+							</div>
 						</div>
-					</div>;
+					);
 				}}
 			</Consumer>
 		);
