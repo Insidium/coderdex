7e9e4cb24d5c4184a327eae6c3dd95013760d39f
Insidium <ryanchallen@outlook.com>
2020-06-16 14:46:44 +1000

    Created 404 Not Found page for unknown routes, added to app.js, styled this page slightly with an icon and title colour

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Contact from './components/contacts/Contact';
 
 function App() {
 	return (
@@ -21,6 +21,7 @@ function App() {
 							<Route exact path='/' component={Contacts}></Route>
 							<Route exact path='/contact/add' component={AddContact}></Route>
 							<Route exact path='/about' component={About}></Route>
+							<Route component={NotFound}></Route>
 						</Switch>
 					</div>
 				</div>
