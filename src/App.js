047d592aca81f320f8f8819b339633c642cce1c8
Insidium <ryanchallen@outlook.com>
2020-06-16 14:08:51 +1000

    Installed react-router-dom for route handling, created About page and assigned add contact form to own route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
-import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Contact from './components/contacts/Contact';
 
 function App() {
 	return (
 		<Provider>
-			<div className='App'>
-				<Header branding='Coderdex' />
-				<div className='container'>
-					<AddContact />
-					<Contacts />
+			<Router>
+				<div className='App'>
+					<Header branding='Coderdex' />
+					<div className='container'>
+						<Switch>
+							<Route exact path='/' component={Contacts}></Route>
+							<Route exact path='/contact/add' component={AddContact}></Route>
+							<Route exact path='/about' component={About}></Route>
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		</Provider>
 	);
 }
