9b552d81f1bdd2ebee645205ab25802a9798205c
Insidium <ryanchallen@outlook.com>
2020-06-18 14:56:43 +1000

    Updates

---
+++ precache-manifest.3c77050b26593b4761066541c746a84e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2153a57fc351e958d54b69cc9e4ae4f1",
+    "url": "/coderdex/index.html"
+  },
+  {
+    "revision": "442d3d9709ebd843af41",
+    "url": "/coderdex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f08c1c33b58538784986",
+    "url": "/coderdex/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "442d3d9709ebd843af41",
+    "url": "/coderdex/static/js/2.95f19acb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coderdex/static/js/2.95f19acb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08c1c33b58538784986",
+    "url": "/coderdex/static/js/main.3cd8ebef.chunk.js"
+  },
+  {
+    "revision": "3643a61c090384f3c1c2",
+    "url": "/coderdex/static/js/runtime-main.07cfd96b.js"
+  }
+]);
